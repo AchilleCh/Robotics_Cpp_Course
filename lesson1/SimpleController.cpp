ef0b02f4587f0349fd417b0624a1ca1961cc8b1c
AchilleCh <achille.chiuchiarelli@gmail.com>
2026-01-30 16:30:55 +0100

    fix: Addition of vel_max variable and minor changes.

--- lesson1/SimpleController.cpp
+++ lesson1/SimpleController.cpp
@@ -16,6 +16,7 @@ int main() {
     // constants defining distance thresholds
     const double stop_distance = 0.3;
     const double slow_down_distance = 1.0;
+    const double vel_max = 0.4;
 
     // First prompt to terminal. Then in the same stream, so no << again and no endl
     std::cout << "Insert obstacle distance (m): ";
@@ -23,9 +24,10 @@ int main() {
 
     while (distance >= stop_distance) {
         if (distance < slow_down_distance) {
-        std::cout << "ACTION: Robot slowing down (0.1m/s)." << std::endl;
+        std::cout << "ACTION: Robot slowing down (0.1 m/s)." << std::endl;
         } else if (distance >= slow_down_distance){
-        std::cout << "ACTION: Robot moving at full speed (0.4 m/s)" << std::endl;
+        // concatenation of strings
+        std::cout << "ACTION: Robot moving at full speed (" << vel_max << " m/s)" << std::endl;
         }
         std::cout << "Insert obstacle distance (m): ";
         std::cin >> distance;
